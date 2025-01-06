2965f5631ca45e98349c1302c5df0ca29d44ae40
Suresh-chatbotDev <suresh@skygoalnext.com>
2025-01-06 17:25:21 +0530

    removed payU hardcode keys in ecom.js and added those in .env

--- utility/ecom.js
+++ utility/ecom.js
@@ -932,9 +932,9 @@ async function refund_transaction(phone, total_amount, transaction) {
     const mihpayid = transaction;
     const token_id = uuid.v4();
     const refund_amount = total_amount;
-    const merchant_key = "kPFnBJ";
-    const salt = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCxQyxletw5kE/0R+8uNEHOobYHYygYpp27YLnf9WAc4sz1+nMMwh9+y4OyTvqvUQh+I7+Lk85XoVVAUDLLu0IsyvJ1hCEQjJH7WkUqk4XZF+r8/X4Fxzr0Wf4CfLZbLn7PO6lOjhGK4bPoViFuQJ8BVtGEhFp6ed169jRGMmTOj44Mno9eFbbzwpB0Rh40SLUuIjf8HGHTX8cJ1vipFNEP7ASH56kHxdOZXDZ35WSjZx2j7VQua5rmCCwRL/XYAkLpzVWLVFTixpUt7U+IBSJ7saTEKfITConb3s6BugNdB4Vnos380hiOi9bS5FIkjrM/GJ5t5+2APJoZyuoaBLJnAgMBAAECggEAe6oqWe25n+se7IQWx/wrANXuYP77JR9wIR4c7rKHx/8uEFkWVItFX7bpfMb+urpkm2OjKOQH6zihegm5NkrAovE+718rlhkLaviSEl7y3P6DsNXESpGwfnId9Gw+6CPq0faEakpQ0LwfP/J+xiUNCOkhqDqRyKomKreCxoo3q6ZvRODXddTQM2u9s8C/1RGM5Utmhu8aJyj88LJS96wiULo/IVR0EaGV6TxGFJHJcJHakN0LaaJtwvW2X0i2H4lpXMfSvr2cRUpsMEG+iuAM/HxAn75LAY25tEn+Pj4M4tWf1iIC3PlN2jKwu2hpo6ZzM5ddlsdgfkgHY76aa9TCoQKBgQDglNkCh+ssMWwV4etGH9yiQKalw7FVirhJkrRg3Lftl8tgk8sOR2aRIZG+2n6IBdbsrP9LDgsAAIs9vICWMNbcKUCVFKo0K1JJC70dLutE+qOrPYH24KWqsc+I9G44MRB8mSqT9v9U2hDbn6/ZN4Hg8e6zRpDxaewcf3aChFZpcQKBgQDKD6SlwTfXA1VY2caDUgiLDDWWioPvk0penwtqDCR7hXNNSUh9FidIGJqU0MCqvGsuENbjGUGPnhWrSUjfcQ/K2mlPycKNJS7e2cCIjK1mFPSdjF+XQ3qHJSgOZ4FtbOPjFhKhLS3Ru9WtV8iZLUiJnNer1l1a5PRp/A7Lcv4tVwKBgDn89RO8OLMOh9QWo4NV0shqXR1MLEvkJ7WHld+03iERIshrIPEs6oTq4BEhpa5Fo7s06C5fD+QOP+XO+HzPW4s5c52K2m/iB7sotsoERWdoOD6NATPXya8LfoTkaFlGAfXKLr5J9p/YNqYe028I8BY/Id1UiTRsnzS0jMsilJVhAoGAHqS+sJCb+lS8Fcx5KaNAPm4sllcNaUDqL21pWrzar4zujpMFlkrMzEdG8jiyb3JBwuu02x4SbkhoOuDTV2ebIIV9ISeVBLjV4eAeLdc/2NJmwpnuSU9nfqVo7L5Px5uS9/Z5/s2OPFeDMVW1y10tugj6QEozQDymwIgEamBXIeMCgYEA34BcE+rd6UHL5te29KAT4A2uanfDxcmJXkHmcleAQ1HOf/Fu5wCLJ7WM8KvoxIN/St2QM45D55vH2i8MwHlcMvEoeBHWxD//0VJN1KSYAg9IHI3zlIEdXIFhPVG5t+ozzO2emlsl1MjPXkF3uDnOnW70VkAuIMYCjjYx09XfsSA=";
-    const refund_webhook_url = "https://www.example.com";
+    const merchant_key = process.env.payU_merchant_key;
+    const salt = process.env.payU_salt;
+    const refund_webhook_url = "https://www.example.com";   // change this to the actual webhook URL later.
     const command = "cancel_refund_transaction";
     const hash = generate_hash(merchant_key, command, mihpayid, salt);
     const url = "https://info.payu.in/merchant/postservice.php";
@@ -964,21 +964,21 @@ async function refund_transaction(phone, total_amount, transaction) {
     }
 }
 
-const MERCHANT_KEY = "kPFnBJ";
-const PAYU_SALT = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCxQyxletw5kE/0R+8uNEHOobYHYygYpp27YLnf9WAc4sz1+nMMwh9+y4OyTvqvUQh+I7+Lk85XoVVAUDLLu0IsyvJ1hCEQjJH7WkUqk4XZF+r8/X4Fxzr0Wf4CfLZbLn7PO6lOjhGK4bPoViFuQJ8BVtGEhFp6ed169jRGMmTOj44Mno9eFbbzwpB0Rh40SLUuIjf8HGHTX8cJ1vipFNEP7ASH56kHxdOZXDZ35WSjZx2j7VQua5rmCCwRL/XYAkLpzVWLVFTixpUt7U+IBSJ7saTEKfITConb3s6BugNdB4Vnos380hiOi9bS5FIkjrM/GJ5t5+2APJoZyuoaBLJnAgMBAAECggEAe6oqWe25n+se7IQWx/wrANXuYP77JR9wIR4c7rKHx/8uEFkWVItFX7bpfMb+urpkm2OjKOQH6zihegm5NkrAovE+718rlhkLaviSEl7y3P6DsNXESpGwfnId9Gw+6CPq0faEakpQ0LwfP/J+xiUNCOkhqDqRyKomKreCxoo3q6ZvRODXddTQM2u9s8C/1RGM5Utmhu8aJyj88LJS96wiULo/IVR0EaGV6TxGFJHJcJHakN0LaaJtwvW2X0i2H4lpXMfSvr2cRUpsMEG+iuAM/HxAn75LAY25tEn+Pj4M4tWf1iIC3PlN2jKwu2hpo6ZzM5ddlsdgfkgHY76aa9TCoQKBgQDglNkCh+ssMWwV4etGH9yiQKalw7FVirhJkrRg3Lftl8tgk8sOR2aRIZG+2n6IBdbsrP9LDgsAAIs9vICWMNbcKUCVFKo0K1JJC70dLutE+qOrPYH24KWqsc+I9G44MRB8mSqT9v9U2hDbn6/ZN4Hg8e6zRpDxaewcf3aChFZpcQKBgQDKD6SlwTfXA1VY2caDUgiLDDWWioPvk0penwtqDCR7hXNNSUh9FidIGJqU0MCqvGsuENbjGUGPnhWrSUjfcQ/K2mlPycKNJS7e2cCIjK1mFPSdjF+XQ3qHJSgOZ4FtbOPjFhKhLS3Ru9WtV8iZLUiJnNer1l1a5PRp/A7Lcv4tVwKBgDn89RO8OLMOh9QWo4NV0shqXR1MLEvkJ7WHld+03iERIshrIPEs6oTq4BEhpa5Fo7s06C5fD+QOP+XO+HzPW4s5c52K2m/iB7sotsoERWdoOD6NATPXya8LfoTkaFlGAfXKLr5J9p/YNqYe028I8BY/Id1UiTRsnzS0jMsilJVhAoGAHqS+sJCb+lS8Fcx5KaNAPm4sllcNaUDqL21pWrzar4zujpMFlkrMzEdG8jiyb3JBwuu02x4SbkhoOuDTV2ebIIV9ISeVBLjV4eAeLdc/2NJmwpnuSU9nfqVo7L5Px5uS9/Z5/s2OPFeDMVW1y10tugj6QEozQDymwIgEamBXIeMCgYEA34BcE+rd6UHL5te29KAT4A2uanfDxcmJXkHmcleAQ1HOf/Fu5wCLJ7WM8KvoxIN/St2QM45D55vH2i8MwHlcMvEoeBHWxD//0VJN1KSYAg9IHI3zlIEdXIFhPVG5t+ozzO2emlsl1MjPXkF3uDnOnW70VkAuIMYCjjYx09XfsSA=";
+const merchant_key = process.env.payU_merchant_key;
+const salt = process.env.payU_salt;
 const PAYU_URL = "https://info.payu.in/merchant/postservice.php?form=2";
 
 function generate_hash(command, var1, salt) {
-    const hash_string = `${MERCHANT_KEY}|${command}|${var1}|${salt}`;
+    const hash_string = `${merchant_key}|${command}|${var1}|${salt}`;
     return crypto.createHash('sha512').update(hash_string).digest('hex');
 }
 
 async function get_transaction_details(txnid) {
     const command = "verify_payment";
-    const hash_value = generate_hash(command, txnid, PAYU_SALT);
+    const hash_value = generate_hash(command, txnid, salt);
 
     const payload = new URLSearchParams({
-        key: MERCHANT_KEY,
+        key: merchant_key,
         command,
         var1: txnid,
         hash: hash_value
